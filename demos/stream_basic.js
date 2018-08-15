6fe405f53d564e9d099b5c061d25fd30f41189e9
dustinpfister <dustin.pfister@gmail.com>
2018-08-15 12:56:20 -0400

    war and peace demo

--- demos/stream_basic.js
+++ demos/stream_basic.js
@@ -3,69 +3,25 @@ stream = require('stream'),
 png = require('pngjs').PNG;
 fs = require('fs');
 
-request('https://i.stack.imgur.com/R3O9s.png?s=64&g=1')
+// requesting "War, and peace" (it's over 500,000 words)
+request('http://www.textfiles.com/etext/FICTION/war_peace_text')
 
-.pipe(new png({
-        filterType: 4
-    }))
+.pipe(new stream.Transform({
 
-.on('parsed', function () {
+        objectMode: true,
+        transform: function (a, en, cb) {
 
-    let img = this;
-    pallet = ' ,.,.,.,:,~,i,1,3,8'.split(','),
-    str = '';
+            console.log(a.toString());
 
-    for (let y = 0; y < this.height; y++) {
-
-        str = '';
-
-        for (let x = 0; x < this.width; x++) {
-
-            let idx = (this.width * y + x) << 2,
-
-            // rgb values
-            r = img.data[idx] / 255,
-            g = img.data[idx + 1] / 255,
-            b = img.data[idx + 2] / 255,
-            val = Math.floor((r + g + b) / 3 * 9);
-
-            str += pallet[val];
+            cb();
 
         }
 
-        console.log(str);
-
-    }
-
-    console.log('image size: ', img.width, img.height);
-
-})
-
-/*
-.pipe(new stream.Transform({
-
-objectMode: true,
-transform: function (a, en, cb) {
-
-console.log(a.toString());
-
-cb();
-
-}
-
-}))
- */
+    }))
 
 .on('error', function (err) {
 
     console.log(err);
 
-})
-
-/*
-.on('response', function (res) {
-
-console.log(res)
-
 });
-*/
+
