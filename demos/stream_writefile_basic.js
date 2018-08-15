1b921534a07ab5eff074b1f734d0ea25231bc408
dustinpfister <dustin.pfister@gmail.com>
2018-08-15 19:52:59 -0400

    basic write file cleanup

--- demos/stream_writefile_basic.js
+++ demos/stream_writefile_basic.js
@@ -4,10 +4,11 @@ fs = require('fs');
 // start the get request
 request(process.argv[2] || 'https://dustinpfister.github.io')
 
-.on('data', function(chunk){
-	
-	console.log(chunk.toString());
-	
-})
+// log any error
+.on('error', function(err){ console.log(err);})
 
+// log chunks
+.on('data', function (chunk) {console.log(chunk.toString());})
+
+// pipe to createWriteStream
 .pipe(fs.createWriteStream('file_' + new Date().getTime() + '.txt'));
